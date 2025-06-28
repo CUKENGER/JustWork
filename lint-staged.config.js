910e3372cb68bb4a55d625df8ccf08419dcd02a9
CUKENGER <andreychuvashov2015@gmail.com>
2025-06-28 21:56:19 +0300

    init

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,10 +3,12 @@ module.exports = {
 		() => 'pnpm --filter frontend typecheck',
 		'pnpm --filter frontend lint',
 		'pnpm --filter frontend format',
+		'git add',
 	],
 	'packages/backend/**/*.{js,ts}': [
 		() => 'pnpm --filter backend typecheck',
 		'pnpm --filter backend lint',
 		'pnpm --filter backend format',
+		'git add',
 	],
 }
