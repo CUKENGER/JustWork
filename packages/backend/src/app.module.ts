34ff16faaa16de9f1da56021d8d635211d88b77f
CUKENGER <andreychuvashov2015@gmail.com>
2025-06-28 21:21:19 +0300

    test

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -5,7 +5,6 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { LoggerModule } from 'nestjs-pino';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from 'exceptions/allExceptionFilter';
-import { UserModule } from 'models/user/user.module';
 
 @Module({
   imports: [
